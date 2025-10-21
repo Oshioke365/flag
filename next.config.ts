a160a577a3a435953554685c69928bb902dad999
Igbanoi oshioke <oshiokeigbanoi@gmail.com>
2025-10-21 17:22:04 +0100

    fi

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // 👈 ensures Next.js produces a static site
   reactStrictMode: true,
   images: {
-    unoptimized: true, // required because GitHub Pages doesn’t support Next.js image optimization
+    unoptimized: true, // 👈 required for GitHub Pages
   },
-  
+  basePath: "/flag", // 👈 replace with your GitHub repo name
+  assetPrefix: "/flag/", // 👈 same here
 };
 
 export default nextConfig;
