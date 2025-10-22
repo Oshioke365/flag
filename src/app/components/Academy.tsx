a51207f9d3e1bd966fb3944841df80d9f983a9ca
Igbanoi oshioke <oshiokeigbanoi@gmail.com>
2025-10-22 04:23:55 +0100

    error

--- src/app/components/Academy.tsx
+++ src/app/components/Academy.tsx
@@ -6,7 +6,10 @@ export default function CFFLAcademyPage() {
   return (
     // The main container: takes up the full viewport height and uses flexbox
     // to center the content (the image) both horizontally and vertically.
-    <div className="flex h-screen w-full items-center justify-center bg-white" >
+    <div className="flex h-screen w-full items-center justify-center bg-white"        style={{
+        position:"absolute",
+        marginTop:"3700px",
+      }}>
       
       {/* Updated Image component with new dimensions */}
       <Image
